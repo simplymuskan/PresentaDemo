32ab1e5bafa0e85bdc5706cc28b32f09ed8b3c91
Muskan <65819611+simplymuskan@users.noreply.github.com>
2023-11-21 19:55:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 import Presenta from 'https://cdn.skypack.dev/@presenta/lib'
 import configInterpolator from 'https://cdn.skypack.dev/@presenta/config-interpolator'
 
-const {interpolate} = configInterpolator
+const { interpolate } = configInterpolator
 
 const update = async () => {
-    console.log("hey")
+  console.log("hey")
   const text = document.querySelector('textarea').value
   const data = text.split('\n')
 
-  const frames = data.map((text,index) => ({text, page: index+1}))
+  const frames = data.map((text, index) => ({ text, page: index + 1 }))
 
-  const config = await interpolate({frames}, 'd0e5e758967cb21cfe88717cf4f36822')
-  
+  const config = await interpolate({ frames }, 'd0e5e758967cb21cfe88717cf4f36822')
+
   document.querySelector('#app').innerHTML = ''
   const preso = await new Presenta('#app', config)
 }
-
+//document.querySelector('req')
 document.querySelector('textarea').addEventListener('change', update)
 
 update()
